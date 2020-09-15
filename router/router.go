507e09f67719b248eb8f2f4ba9f6318b1d6f8a1a
higashi000 <higashi.ict@gmail.com>
2020-09-15 15:43:17 +0900

    Add test server

--- router/router.go
+++ router/router.go
@@ -1,11 +1,20 @@
 package router
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/kr/pretty"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+type routeRequest struct {
+	Lat   float64 `json:"latitude"`
+	Long  float64 `json:"longitude"`
+	Label string  `json:"label"`
+}
 
 func NewRouter() *echo.Echo {
 	e := echo.New()
@@ -22,6 +31,30 @@ func NewRouter() *echo.Echo {
 		}
 
 		c.JSON(http.StatusOK, `{"status": "OK", "lat": 26.5263, "long": 128.031}`)
+
+		return nil
+	})
+
+	e.GET("/route", func(c echo.Context) error {
+		var routeData routeRequest
+		err := c.Bind(&routeData)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, `{"status": "error bind json"}`)
+			return nil
+		}
+
+		jsonFile, err := ioutil.ReadFile("./data.json")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, `{"status": "error read file"}`)
+			return nil
+		}
+		route := []routeRequest{}
+
+		err = json.Unmarshal(jsonFile, &route)
+
+		pretty.Println(route)
+
+		c.JSON(http.StatusOK, route)
 
 		return nil
 	})
